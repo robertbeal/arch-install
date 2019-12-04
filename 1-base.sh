082cdcaae8c1e6bca4d28b93cc2bfe25d0785e44
Rob Beal <rob@kohi.uk>
2019-12-04 22:59:15 +0000

    Initial commit

---
+++ 1-base.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+# change to UK keyboard (arch defaults to US)
+loadkeys uk
+
+# connect to wifi...
+wifi-menu
+
+# create partitions
+cgdisk /dev/sdx
+1 100MB EFI partition # Hex code = ef00
+2 100% / partition    # Hex code = 8300
+
+# format the EFI partition
+mkfs.vfat -F32 /dev/sdx1
+
+# create the encrypted partition on partition 1
+cryptsetup --verify-passphrase luksFormat /dev/sdx2 --type luks1 --cipher aes-xts-plain64 -s 512 -h sha512 --iter-time 100 --key-slot 1
+# open the encrypted partition
+cryptsetup luksOpen /dev/sdx2 cryptroot
+
+# encryption key for grub (so passphrase isn't prompted twice)
+# put it on slot 0 for added boot speed
+dd if=/dev/urandom of=/crypto_keyfile.bin bs=1024 count=4
+chmod 000 /crypto_keyfile.bin
+cryptsetup luksAddKey /dev/sdx2 /crypto_keyfile.bin --key-slot 0
+
+# logical volumes inside the encrypted partition
+pvcreate /dev/mapper/cryptroot
+vgcreate system /dev/mapper/cryptroot
+lvcreate --size 8G system --name swap
+lvcreate -l +100%FREE system --name root
+
+# create filesystems on encrypted partitions
+mkfs.ext4 /dev/mapper/system-root
+mkswap /dev/mapper/system-swap
+
+# mount /
+mount /dev/mapper/system-root /mnt
+swapon /dev/mapper/system-swap
+# mount efi partiition
+mkdir -p /mnt/boot/efi
+mount /dev/sdx1 /mnt/boot/efi
+
+# install base system
+pacstrap /mnt \
+    base \
+    base-devel \
+    bash \
+    efibootmgr \
+    grub-efi-x86_64 \
+    intel-ucode
+
+# generate fstab
+genfstab -pU /mnt >>/mnt/etc/fstab
+# For SSD's change 'relatime' on all non-boot partitions to 'noatime' (reduces wear)
+
+# copy the Grub crypto key into the root partition
+cp /crypto_keyfile.bin /mnt
+
+# enter the new system
+arch-chroot /mnt /bin/bash
+
+# install base packages
+pacman -S \
+    dialog \
+    gnome-terminal \
+    linux \
+    linux-firmware \
+    mesa \
+    nano \
+    sudo \
+    vim \
+    wpa_supplicant \
+    xf86-video-fbdev \
+    xorg-server
+
+# mkinitcpio
+vim /etc/mkinitcpio.conf
+## Add 'keyboard keymap' to HOOKS before 'block'
+## Add 'encrypt lvm2' to HOOKS before 'filesystems'
+sed -i 's\^FILES=.*\FILES="/crypto_keyfile.bin"\g' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
+# grub
+vim /etc/default/grub
+# GRUB_HIDDEN_TIMEOUT=5
+# GRUB_HIDDEN_TIMEOUT_QUIET=true
+# GRUB_ENABLE_CRYPTODISK=y
+# GRUB_CMDLINE_LINUX="cryptdevice=/dev/sdx2:cryptroot root=/dev/mapper/system-root"
+# GRUB_CMDLINE_LINUX="cryptdevice=UUID=...:cryptroot root=UUID=..."
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Arch"
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# users
+useradd --create-home --user-group --group wheel rob && passwd rob
+sed -i '/%wheel ALL=(ALL) ALL/s/^#//' /etc/sudoers # allow wheel group in sudoers
+passwd -l root                                     # disable root password
+
+# hostname
+echo "arch-linux" >/etc/hostname
+
+# system clock
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+hwclock --systohc --utc
+
+# locale
+vim /etc/locale.gen # uncomment any locales needed, ie en_GB.UTF-8
+locale-gen
+echo LANG=en_GB.UTF-8 >/etc/locale.conf
+
+# time
+pacman -S --noconfirm ntp
+systemctl enable ntpd
+
+# pacman
+pacman -S --noconfirm pacman-contrib
+systemctl enable paccache.timer
+
+# login
+pacman -S lightdm lightdm-gtk-greeter
+systemctl enable lightdm
+
+# desktop (cinnamon)
+pacman -S cinnamon nemo-fileroller nemo-preview
+
+# networkManager
+pacman -S networkmanager gnome-keyring
+systemctl enable NetworkManager
+
+### hardening
+chmod 700 /boot /etc/iptables
+
+# clean up and reboot
+exit
+umount -R /mnt
+swapoff -a
---
+++ 2-common.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+# kernel hardening
+cat sysctl.conf | sudo tee /etc/sysctl.d/51-net.conf
+
+# dock + theme
+sudo pacman -S --noconfirm \
+    arc-gtk-theme \
+    elementary-icon-theme \
+    plank \
+    rofi
+
+# firewall
+sudo pacman -S --noconfirm ufw ufw-extras
+sudo systemctl enable ufw.service
+sudo ufw --force reset
+sudo ufw allow ntp
+sudo ufw --force default deny incoming
+sudo ufw --force default allow outgoing
+sudo ufw --force enable
+
+# common system apps
+sudo pacman -S --noconfirm \
+    bind-tools \
+    code \
+    cronie \
+    dconf-editor \
+    dosfstools \
+    fakeroot \
+    gparted \
+    net-tools \
+    openssh \
+    patch
+
+# gnome apps
+sudo pacman -S --noconfirm \
+    eog \
+    evince \
+    gnome-packagekit \
+    gnome-calculator \
+    gnome-screenshot \
+    gthumb \
+    rhythmbox
+
+# entropy
+sudo pacman -S --noconfirm rng-tools haveged
+sudo systemctl enable haveged
+sudo systemctl enable rngd
+
+# dev
+sudo pacman -S --noconfirm \
+    curl \
+    git \
+    jq \
+    shellcheck \
+    stow \
+    tmux \
+    vim \
+    zsh
+
+# web
+sudo pacman -S --noconfirm \
+    firefox \
+    chromium \
+    mpv
+
+# office
+sudo pacman -S --noconfirm \
+    libreoffice-fresh \
+    mythes-en \
+    hyphen-en \
+    hyphen \
+    libmythes
+
+# android
+sudo pacman -S --noconfirm \
+    libmtp \
+    mtpfs \
+    android-udev \
+    android-tools \
+    gvfs \
+    gvfs-mtp
+
+# fonts
+## dejavu
+sudo pacman -S --noconfirm ttf-dejavu ttf-liberation noto-fonts &&
+sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d &&
+sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d &&
+sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d &&
+sudo sed -i 's/^#export FREETYPE_PROPERTIES/export FREETYPE_PROPERTIES/' /etc/profile.d/freetype2.sh &&
+cat fontconfig.xml | sudo tee /etc/fonts/local.conf &&
+## msfonts
+curl -L https://github.com/robertbeal/msfonts/raw/master/install.sh | sudo sh &&
+## google fonts
+curl https://raw.githubusercontent.com/qrpike/Web-Font-Load/master/install.sh | bash;
+
+# network manager
+sudo pacman -S --noconfirm dnsmasq networkmanager-openvpn network-manager-applet libsecret
+echo "[main]
+dns=dnsmasq" | sudo tee /etc/NetworkManager/NetworkManager.conf
+
+# printing
+sudo pacman -S --noconfirm cups cups-pdf system-config-printer gtk3-print-backends ghostscript gsfonts gutenprint
+sudo pacman -S --noconfirm hplip
+sudo systemctl enable org.cups.cupsd.service
+sudo systemctl start org.cups.cupsd.service
+sudo groupadd printadmin
+sudo usermod -aG printadmin "$USER"
+sudo sed -i "/SystemGroup sys root$/c\SystemGroup sys root printadmin" /etc/cups/cups-files.conf
+
+# syncthing
+sudo pacman -S --noconfirm syncthing syncthing-gtk
+systemctl enable syncthing --user
+
+# settings
+gsettings set org.cinnamon.desktop.wm.preferences theme 'Arc Dark'
+gsettings set org.cinnamon.settings-daemon.peripherals.touchpad natural-scroll false
+gsettings set org.cinnamon.settings-daemon.peripherals.touchpad tap-to-click true
+gsettings set org.cinnamon.desktop.wm.preferences num-workspaces 3
+gsettings set org.nemo.preferences default-folder-viewer 'list-view'
+gsettings set org.cinnamon.settings-daemon.plugins.power idle-dim-time 90
+gsettings set org.cinnamon.settings-daemon.plugins.power sleep-inactive-ac-timeout 1800
+gsettings set org.cinnamon.settings-daemon.plugins.power sleep-inactive-battery-timeout 900
+gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-ac 600
+gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-battery 300
+gsettings set org.cinnamon.settings-daemon.plugins.power idle-brightness 10
+gsettings set org.cinnamon.settings-daemon.plugins.power sleep-inactive-battery-type 'suspend'
+gsettings set org.cinnamon.settings-daemon.plugins.power sleep-inactive-ac-type 'suspend'
+gsettings set org.gnome.nm-applet disable-disconnected-notifications true
+gsettings set org.gnome.nm-applet disable-vpn-notifications true
+gsettings set org.gnome.nm-applet disable-connected-notifications true
+gsettings set org.gnome.nm-applet suppress-wireless-networks-available true
+gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+
+# ssh permissions
+chmod 0700 ~/.ssh
+find ~/.ssh -type d -exec chmod 0700 {} +
+find ~/.ssh -type f -exec chmod 0600 {} +
+chmod -R 0644 ~/.ssh/*.pub
+
